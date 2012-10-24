b0f99bc0cb71114a2415c2b986582517773aed5c
Simon Urbanek <simon.urbanek@r-project.org>
2012-10-24 19:27:57 +0000

    add rfBin

---
+++ R/rfBin.R
@@ -0,0 +1,10 @@
+rfBin <- function(filename, what=1L, ...) {
+  if (!is.character(filename)) stop("filename must be a character vector")
+  con <- file(filename, "rb")
+  on.exit(close(con))
+  sm <- match(storage.mode(what), c("integer", "double"))
+  if (any(is.na(sm))) stop("invalid `what' - must be a double or interger")
+  sz <- c(4L, 8L)[sm]
+  n <- file.info(filename)$size / sz
+  readBin(con, what, n, ...)
+}
